2f5d678d80cb6fe3cf2a8ba259a791ef7f468114
jogava <jgv.089@gmail.com>
2024-10-14 13:01:59 -0300

    Anular Inmediato y corrección Inmediato

--- cypress/e2e/2-Traslado/AnularTrasladoInmediato.cy.ts
+++ cypress/e2e/2-Traslado/AnularTrasladoInmediato.cy.ts
@@ -12,9 +12,12 @@ describe('Siniestros', () => {
     cy.get('[ng-reflect-router-link="gestion,siniestros"]').click();
     cy.get('.col-10 > .row > :nth-child(2) > .form-control').type('197928').wait(2000);
     cy.get(':nth-child(3) > :nth-child(1) > a').click().wait(9999);
-    cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1) > .float-left > .btn').click().wait(1000);
+    cy.get(':nth-child(1) > .acciones > acciones-traslado > .btn-group > #button-basic').click();
+    cy.get(':nth-child(4) > .dropdown-item').click();
+    cy.get('.ng-star-inserted > .form-control > .ng-select-container > .ng-value-container > .ng-input').click();
+    cy.get('span[ng-reflect-ng-item-label="Decisión operativa"]').click();
+    cy.get('.firstinfo > :nth-child(2) > .form-control').type('Se anula desde Automation');
     cy.get('.modal-footer > button.btn').click();
-    cy.get('.float-right > custom-switch.ng-valid > .custom-control > .custom-control-label').click();
-    cy.get('form.ng-untouched > .modal-footer > button.btn').click();
+
   }); 
 });
